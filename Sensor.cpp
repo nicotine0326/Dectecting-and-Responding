2e1eaea0cad0f085523fab7915d4b850e47f8c7d
nicotine0326 <dongpham1612@gmail.com>
2025-12-17 00:44:07 +0700

    Refactor Sensor.cpp and update output messages
    Updated the display message for distance readings and improved code formatting.

--- Sensor.cpp
+++ Sensor.cpp
@@ -1,47 +1,33 @@
 #include <Adafruit_LiquidCrystal.h>
-
 Adafruit_LiquidCrystal lcd(0);
-
-
-
 const int trigPin = 9; 
 const int echoPin = 10;
 float duration, cm, inches;
-
-
-
 void setup() {
-
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   lcd.begin(16, 2);
   Serial.begin(9600); 
-
 }
-
-
-
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
   duration = pulseIn(echoPin, HIGH, 25000); 
   cm = (duration / 2) / 29.1;
-  
-  Serial.print("Dang do duoc: ");
+  Serial.print("Currently: ");
   Serial.println(cm);
   if (cm < 50) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Hello");
-  } else {
+  } 
+  else {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Qua gioi han");
+    lcd.print("Too far!");
   }
-  
-  delay(200);
+   delay(200);
 }
